7e9e98207097ff6b4905ff6a346d1922fa6eb8ab
Nicolas Partridge <nicolas@Nicolass-MacBook-Pro.local>
2019-06-25 13:45:59 -0700

    Updates

---
+++ precache-manifest.994791e5cf0c5d69a23f5dec4bec4533.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d27b84b641940a2d842ee6831df532d",
+    "url": "/cyrillic/index.html"
+  },
+  {
+    "revision": "3a6a5a01c59a37dc2ced",
+    "url": "/cyrillic/static/css/main.b03d4cbb.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/cyrillic/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "3a6a5a01c59a37dc2ced",
+    "url": "/cyrillic/static/js/main.8e0408ea.chunk.js"
+  },
+  {
+    "revision": "7f007428e4e54f4d6c37",
+    "url": "/cyrillic/static/js/runtime~main.0b418cd9.js"
+  }
+]);
